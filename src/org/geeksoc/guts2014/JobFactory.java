d0b5502ec220672ba6f600aa3e585bdb1122efa9
Ally <aggenkeech@gmail.com>
2014-10-10 23:02:43 +0100

    Some job code

---
+++ src/org/geeksoc/guts2014/JobFactory.java
@@ -0,0 +1,21 @@
+package org.geeksoc.guts2014;
+
+import java.awt.event.ActionListener;
+import java.util.Timer;
+
+public class JobFactory {
+	
+	
+
+	public static void main(String[] args) {
+		Timer timer = new Timer(500, new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				// 
+			}
+		})
+	}
+	
+ // updateTime
+	
+}
