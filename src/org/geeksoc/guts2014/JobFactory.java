598c4b3d584059d867ee1d2d75e25d95b8158249
Ally <aggenkeech@gmail.com>
2014-10-11 00:57:19 +0100

    Fixing up some code

--- src/org/geeksoc/guts2014/JobFactory.java
+++ src/org/geeksoc/guts2014/JobFactory.java
@@ -10,30 +10,34 @@ public class JobFactory {
 	
 	private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 	private static ArrayList<Job> jobs;
+	private static boolean isRunning;
 	
 	
 	public static void main(String[] args) {
+		isRunning = true;
 		jobs = new ArrayList<Job>();
+	}
+	
+	public void startJobCreation() {
+		isRunning = true;
 		createJobs();
 	}
 	
-	public static void createJobs() {
-		final Runnable beeper = new Runnable() {
-			public void run() { 
-				jobs.add(new Job());
-			}
-		};
-		
-		
-		ScheduledFuture<?> handler;
-		
-		
-		final ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(beeper, 10, 10, TimeUnit.SECONDS);
-		scheduler.schedule(new Runnable() {
-			public void run() {
-				beeperHandle.cancel(true); 
-			}
-		}, 60 * 60, TimeUnit.SECONDS);
+	public void cancelJobCreation() {
+		isRunning = false;
+	}
+	
+	public void clearJobs() {
+		Main.jobQueue.clear();
+	}
+	
+	public void createJobs() {
+		if(!isRunning) {
+			
+			// randomly create a job
+			Main.jobQueue.add(new Job());
+			
+		}
 	}
 		
 		
--- src/org/geeksoc/guts2014/Main.java
+++ src/org/geeksoc/guts2014/Main.java
@@ -1,15 +1,33 @@
 package org.geeksoc.guts2014;
 
+import java.util.ArrayList;
+
 import org.newdawn.slick.AppGameContainer;
 import org.newdawn.slick.SlickException;
 
 public class Main {
 	
+	public static ArrayList<Job> jobQueue;
+	
 	public static void main(String[] args) throws SlickException {
+		
+		jobQueue = new ArrayList<Job>();
+		JobFactory jobFactory = new JobFactory();
+		
+		// Will add random job at random time interval to jobQueue of type Job
+		jobFactory.startJobCreation();
+		// do not call this at update??
+		
+		// Will stop creating jobs, and empty the jobQueue
+		jobFactory.startJobCreation();
+		
+		
 		
 		AppGameContainer game = new AppGameContainer(new Game("geeksoc"));
 		
 		game.start();
 	}
+	
+	
 
 }
