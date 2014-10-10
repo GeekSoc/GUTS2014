36984e50624365e17b5d3d1a8459ba7122e490d0
Ally <aggenkeech@gmail.com>
2014-10-11 00:05:49 +0100

    Working on random job creation event

--- src/org/geeksoc/guts2014/Job.java
+++ src/org/geeksoc/guts2014/Job.java
@@ -1,5 +1,8 @@
 package org.geeksoc.guts2014;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 import java.util.Random;
 
 public class Job {
@@ -7,17 +10,19 @@ public class Job {
 	private static Random random = new Random();
 
 	// Attributes
-	private static String[] jobTypes = { "PHONE", "EMAIL", "WEBCHAT", "SOCIAL" };
-	private static String jobType;
+	private static JobType jobType;
 	private static int jobDifficulty;
 	
 	
-	public Job() {
-		jobType = jobTypes[randomNumber(0, 3)];
+	public Job Job() {
+		List<JobType> jobTypes = Collections.unmodifiableList(Arrays.asList(JobType.values()));
+		jobType = jobTypes.get(randomNumber(0, jobTypes.size()));
 		jobDifficulty = randomNumber(0,100);
+		return this;
 	}
 	
-	public static String getJobType() {
+	
+	public static JobType getJobType() {
 		return jobType;
 	}
 	
--- src/org/geeksoc/guts2014/JobFactory.java
+++ src/org/geeksoc/guts2014/JobFactory.java
@@ -1,20 +1,42 @@
 package org.geeksoc.guts2014;
 
-import java.awt.event.ActionListener;
-import java.util.Timer;
+import java.util.ArrayList;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
 
 public class JobFactory {
 	
+	private final static ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+	private static ArrayList<Job> jobs;
 	
-
+	
 	public static void main(String[] args) {
-		Timer timer = new Timer(500, new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// 
-			}
-		})
+		jobs = new ArrayList<Job>();
+		createJobs();
 	}
+	
+	public static void createJobs() {
+		final Runnable beeper = new Runnable() {
+			public void run() { 
+				jobs.add(new Job());
+			}
+		};
+		
+		
+		ScheduledFuture<?> handler;
+		
+		
+		final ScheduledFuture<?> beeperHandle = scheduler.scheduleAtFixedRate(beeper, 10, 10, TimeUnit.SECONDS);
+		scheduler.schedule(new Runnable() {
+			public void run() {
+				beeperHandle.cancel(true); 
+			}
+		}, 60 * 60, TimeUnit.SECONDS);
+	}
+		
+		
 	
  // updateTime
 	
