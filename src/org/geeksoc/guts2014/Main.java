cd72787b255d4305ab9f5825458840237651139f
Qweasdy <derekclark1995@yahoo.co.uk>
2014-10-10 23:21:55 +0100

    I was told to write a message here

--- src/org/geeksoc/guts2014/Main.java
+++ src/org/geeksoc/guts2014/Main.java
@@ -1,5 +1,12 @@
 package org.geeksoc.guts2014;
 
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.SlickException;
+
 public class Main {
+	
+	public static void main(String[] args) throws SlickException {
+		AppGameContainer game = new AppGameContainer(new Game("geeksoc"));
+	}
 
 }
