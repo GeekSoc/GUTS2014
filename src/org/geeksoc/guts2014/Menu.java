00298c9b1efab94216b7ff60359e033ef5fe5f6a
Robert Wilson <robert.wilson1717@gmail.com>
2014-10-11 06:51:40 +0100

    Why Not :)

--- src/org/geeksoc/guts2014/Menu.java
+++ src/org/geeksoc/guts2014/Menu.java
@@ -1,8 +1,10 @@
 package org.geeksoc.guts2014;
 
+import org.newdawn.slick.Animation;
 import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Rectangle;
 import org.newdawn.slick.state.BasicGameState;
@@ -17,6 +19,7 @@ public class Menu extends BasicGameState {
 	private boolean clicked = false;
 	private int mouseX = 0;
 	private int mouseY = 0;
+	private Animation phone;
 
 	public Menu() {
 
@@ -27,10 +30,15 @@ public class Menu extends BasicGameState {
 
 		int x1 = (container.getWidth() / 2) - (buttonWidth / 2);
 		startButton = new Rectangle(x1, 100, buttonWidth, buttonHeight);
-		Music Sound_1 = new Music("res/sound/background.ogg");
+		//Music Sound_1 = new Music("res/sound/background.ogg");
 		//Sound_1.loop(1.0f, 1.0f);
 		//Fuck off wit hte annoying music for now :(
-
+		Image phone1 = new Image("res/img/phone.gif");
+		Image phone2 = new Image("res/img/phone2.gif");
+		Image phone3 = new Image("res/img/phone3.gif");
+		Image[] phones = new Image[]{phone1,phone2,phone3};
+		phone = new Animation(phones,1);
+		
 	}
 
 	public void update(GameContainer container, StateBasedGame game, int delta)
@@ -52,11 +60,20 @@ public class Menu extends BasicGameState {
 
 	public void render(GameContainer container, StateBasedGame game, Graphics g)
 			throws SlickException {
+		g.scale(2, 2);
+		g.drawAnimation(phone, 10, 10);
+		for(int x = 0; x<5; x++){
+			for(int y = 0; y<5; y++){
+				g.drawAnimation(phone, (x*60)+10, (y*40)+10);
+			}
+		}
+		
+		g.scale(1, 1);
 		g.setColor(Color.white);
 		g.fill(startButton);
 		g.setColor(Color.black);
 		g.drawString("Start", startButton.getCenterX()-20, startButton.getCenterY());
-
+		
 	}
 
 	public void mouseClicked(int button, int x, int y, int clickCount) {
