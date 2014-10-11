0ae330581c36f5fceee81d5451362a930bf76e96
Qweasdy <derekclark1995@yahoo.co.uk>
2014-10-11 01:42:51 +0100

    added RoomRenderer

---
+++ src/org/geeksoc/guts2014/RoomRenderer.java
@@ -0,0 +1,17 @@
+package org.geeksoc.guts2014;
+
+import org.newdawn.slick.Graphics;
+
+public class RoomRenderer {
+	
+	Graphics graphics;
+	
+	public RoomRenderer(Graphics g) {
+		graphics = g;
+	}
+
+	public void render(int nRooms, int[] people) { // nrooms is the number of rooms and 'people'
+		                                           // is an array containing number in room
+		
+	}
+}
