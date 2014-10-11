6b73636a502dcdf6923c227bb4f6d474b5ab1c5e
Robert Wilson <robert.wilson1717@gmail.com>
2014-10-11 01:40:22 +0100

    I dinno where it went

---
+++ src/org/geeksoc/guts2014/RunningState.java
@@ -0,0 +1,36 @@
+package org.geeksoc.guts2014;
+
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.StateBasedGame;
+
+public class RunningState extends BasicGameState {
+	
+	private int id;
+	
+	public RunningState(int idee) {
+		id=idee;
+	}
+
+	public void init(GameContainer container, StateBasedGame game)
+			throws SlickException {
+		
+	}
+
+	public void update(GameContainer container, StateBasedGame game, int delta)
+			throws SlickException {
+		
+	}
+	
+	public void render(GameContainer container, StateBasedGame game, Graphics g)
+			throws SlickException {
+		
+	}
+
+	public int getID() {
+		return 1;
+	}
+
+}
